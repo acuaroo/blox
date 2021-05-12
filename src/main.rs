93e80561696a073d71b85bf8684d01d149e022e7
Acuaro <>
2021-05-12 20:12:48 +0000

    Added dir validator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::env;
+use colored::*;
+use std::path::Path;
+
+// Main function
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let dir = &args[2];
+
+    if query == "h" {
+      // "BLOX - Rojo & General Use initializer\n-r: Rojo setup\n-n: Normal setup"
+
+      println!("{} {}", "Blox - Rojo & General Use initializer\n".green().bold(), "r: Rojo setup\n n: Normal setup\n h: Help command".blue().bold());
+    } else if query == "r" {
+      let b: bool = Path::new(dir).is_dir();
+
+      if b == true {
+        println!("{} is a valid dir", dir.green().bold())
+        // TODO: Write rojo configurator here
+        
+      } else {
+        println!("{} {} is not a valid dir", "ERR:".red().bold(), dir.yellow().bold())
+      }
+      
+    } else if query == "n" {
+      // TODO: Write normal configurator here
+    }
 }
