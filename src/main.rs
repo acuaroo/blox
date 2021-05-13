ae720eda1e7fedb803327f748cbd5df126e54887
acuaro-codes <67112172+acuaro-codes@users.noreply.github.com>
2021-05-13 07:32:28 -0600

    Added temp logic for dir finding

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::env;
 use colored::*;
 use std::path::Path;
+use std::fs::File;
+use std::io::prelude::*;
+
+static GITIGNORE: &str = ".DS_Store\ntests/";
+
 
 // Main function
 fn main() {
@@ -17,8 +22,23 @@ fn main() {
       let b: bool = Path::new(dir).is_dir();
 
       if b == true {
-        println!("{} is a valid dir", dir.green().bold())
-        // TODO: Write rojo configurator here
+        // let c_dir = Path::new(dir);
+        // let git_dir = dir.concat("");
+        // let gitig = Path::new("{}");
+        // let display = gitig.display();
+
+        // println!("{} is a valid dir", dir.green().bold());
+
+        // let mut file = match File::create(&gitig) {
+        //   Err(why) => panic!("Couldn't create {}: {}", display, why),
+        //   Ok(file) => file,
+        // };
+  
+        // // Write the `LOREM_IPSUM` string to `file`, returns `io::Result<()>`
+        // match file.write_all(GITIGNORE.as_bytes()) {
+        //     Err(why) => panic!("Couldn't write to {}: {}", display, why),
+        //     Ok(_) => println!("Successfully wrote to {}", display),
+        // }
         
       } else {
         println!("{} {} is not a valid dir", "ERR:".red().bold(), dir.yellow().bold())
