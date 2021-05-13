2bd1eba68bb78fa7700970c969995681386e3bcc
Acuaro <>
2021-05-13 14:57:16 +0000

    Added path joiner + gitignore creation

--- src/main.rs
+++ src/main.rs
@@ -22,23 +22,20 @@ fn main() {
       let b: bool = Path::new(dir).is_dir();
 
       if b == true {
-        // let c_dir = Path::new(dir);
-        // let git_dir = dir.concat("");
-        // let gitig = Path::new("{}");
-        // let display = gitig.display();
+        let gitignore_f = Path::new(dir).join(".gitignore");
+        let git_display = gitignore_f.display();
 
-        // println!("{} is a valid dir", dir.green().bold());
+        println!("{} is a valid dir", dir.green().bold());
 
-        // let mut file = match File::create(&gitig) {
-        //   Err(why) => panic!("Couldn't create {}: {}", display, why),
-        //   Ok(file) => file,
-        // };
-  
-        // // Write the `LOREM_IPSUM` string to `file`, returns `io::Result<()>`
-        // match file.write_all(GITIGNORE.as_bytes()) {
-        //     Err(why) => panic!("Couldn't write to {}: {}", display, why),
-        //     Ok(_) => println!("Successfully wrote to {}", display),
-        // }
+        let mut file = match File::create(&gitignore_f) {
+          Err(why) => panic!("Couldn't create {}: {}", git_display, why),
+          Ok(file) => file,
+        };
+
+        match file.write_all(GITIGNORE.as_bytes()) {
+            Err(why) => panic!("Couldn't write to {}: {}", git_display, why),
+            Ok(_) => println!("Successfully wrote to {}", git_display),
+        }
         
       } else {
         println!("{} {} is not a valid dir", "ERR:".red().bold(), dir.yellow().bold())
